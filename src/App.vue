d89c9fb54d05733643f21063dd2aaa5a68e9eb9a
Jerick-CM <jmangaluz@gmail.com>
2021-07-12 16:03:41 +0800

    imlpement  vuetify.js alpha for vue3

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,67 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link :to="{ name: 'user', params: { username: 'Jerick' } }"
-      >Dynamic Route
-    </router-link>
-  </div>
-  <router-view />
+  <v-app>
+    <v-app-bar fixed app>
+        <v-btn color="blue" class="pa-1 ma-1" ><router-link to="/">Homepage</router-link></v-btn>  |
+        <v-btn color="blue" class="pa-1 ma-1" > <router-link to="/about">About</router-link></v-btn>  |          
+        <v-btn color="blue" class="pa-1 ma-1 white--text" >
+            <router-link :to="{ name: 'user', params: { username: 'User1' } }">
+                Dynamic Routes
+            </router-link>
+        </v-btn> 
+    </v-app-bar>
+    <v-main>
+      <router-view/>
+    </v-main>
+
+
+    <v-footer
+      dark
+      padless
+    >
+      <v-card
+        class="flex"
+        flat
+        tile
+      >
+        <v-card-title class="teal">
+          <strong class="subheading">Get connected with us on social networks!</strong>
+  
+          <v-spacer></v-spacer>
+  
+          <v-btn
+            v-for="icon in icons"
+            :key="icon"
+            class="mx-4"
+            dark
+            icon
+          >
+            <v-icon size="24px">
+              {{ icon }}
+            </v-icon>
+          </v-btn>
+        </v-card-title>
+  
+        <v-card-text class="py-2 white--text text-center">
+          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
+
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  data: () => ({
+   icons: [
+      'mdi-facebook',
+      'mdi-twitter',
+      'mdi-linkedin',
+      'mdi-instagram',
+    ],
+  }),
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <v-btn class="default">test</v-btn>
   </div>
 </template>
